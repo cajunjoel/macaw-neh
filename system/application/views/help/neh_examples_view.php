52a463af51866a7fb27c03be49fec9b1ea60c23f
Joel Richard <github@joelrichard.com>
2014-02-20 11:51:07 -0500

    Reworked the help examples page with new images and content.

--- system/application/views/help/neh_examples_view.php
+++ system/application/views/help/neh_examples_view.php
@@ -1,64 +1,97 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-        "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01//EN"
+       "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
 	<title>Help | Macaw</title>
 	<? $this->load->view('global/head_view') ?>
 </head>
-<body class="yui-skin-sam">
-	<h2 style="color:#4F7E97">Macaw Art of Life Help: Image Examples</h2>
+<body class="yui-skin-sam" style="padding: 3em">
+	<h1 xxxxx">Macaw Art of Life Help: Image Examples</h1>
 	
-	<div style="text-align: left; margin: 20px;">
-		<h3 style="color:#4F7E97">Illustration</h3>
-		<p>This will be the broadest class type.  The majority of pages in the BHL will fall under this type.  It will include drawings, paintings, prints, decorative letters, book plates, advertisements, etc.  If there is every uncertainty as to which type a page should be assigned.  The type of "illustration" should be the default.</p>
-		<p>Illustrations should be identified as being “b&w” or “color”. If a page contains any amount of color, even if minimal, it should be assigned to color</p>
-		<table cellspacing="0" cellpadding="5" align="center">
-			<tr>
-				<td><a href="/images/examples-lg/illustration-1.jpg" target="_blank"><img src="/images/examples-sm/illustration-1.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/illustration-2.jpg" target="_blank"><img src="/images/examples-sm/illustration-2.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-			</tr>
-		</table>
+	<p style="text-align:center;font-weight:bold"><b>Examples of Illustration Class Types </b></p>
+	<h2 style="color:#4F7E97;margin-top:3em">Illustrations</h2>
+	<p>This will be the broadest class type. The majority of pages in the BHL will fall under this type. It will include drawings, paintings, prints, decorative letters, book plates, advertisements, etc. If there is every uncertainty as to which type a page should be assigned. The type of"illustration" should be the default. </p>
+	<table cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td><a href="http://biodiversitylibrary.org/page/7220979" target="_blank"><img width=165 height=100 src="/images/examples/image002.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/2836705" target="_blank"><img border=0 width=146 height=116 src="/images/examples/image004.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/2919109" target="_blank"><img border=0 width=112 height=181 src="/images/examples/image006.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/42195930" target="_blank"><img border=0 width=93 height=165 src="/images/examples/image008.png"></a></td>
+		</tr>
+	</table>
 
-		<h3 style="color:#4F7E97">Diagram / Chart</h3>
-		<p>This type includes diagrams, charts, and tables.  The data is usually laid out in a grid like pattern and will usually be accompanied by a description or caption beginning with the words "figs", "table"  </p>
-		<table cellspacing="0" cellpadding="5" align="center">
-			<tr>
-				<td><a href="/images/examples-lg/diagram-1.jpg" target="_blank"><img src="/images/examples-sm/diagram-1.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/diagram-2.jpg" target="_blank"><img src="/images/examples-sm/diagram-2.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/diagram-3.jpg" target="_blank"><img src="/images/examples-sm/diagram-3.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/diagram-4.jpg" target="_blank"><img src="/images/examples-sm/diagram-4.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/diagram-5.jpg" target="_blank"><img src="/images/examples-sm/diagram-5.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-			</tr>
-		</table>
+	<h2 style="color:#4F7E97;margin-top:3em">Diagrams/Charts</h2>
+	<p>This type includes diagrams, charts, and tables. The data is usually laid out in a grid like pattern and will usually be accompanied by a description or caption beginning with the words"figs","table" </p>
+	<table cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td><a href="http://biodiversitylibrary.org/page/41056131" target="_blank"><img border=0 width=111 height=161 src="/images/examples/image010.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/41055986" target="_blank"><img border=0 width=111 height=158 src="/images/examples/image012.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/41056074" target="_blank"><img border=0 width=118 height=169 src="/images/examples/image014.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/41055977" target="_blank"><img border=0 width=172 height=123 src="/images/examples/image016.png"></a></td>
+			<td><a href="http://biodiversitylibrary.org/page/16016865" target="_blank"><img border=0 width=103 height=168 src="/images/examples/image018.png"></a></td>
+		</tr>
+	</table>
+
+	<h2 style="color:#4F7E97;margin-top:3em">Maps</h2>
+	<p>Maps are any illustration that conveys geographic information and will usually be accompanied by a description or caption beginning with the word"map" but could also include"figure" </p>
+	<table cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td width="25%" valign="top" style="text-align:center"><a href="http://biodiversitylibrary.org/page/41055982" target="_blank"><img border=0 width=110 height=156 src="/images/examples/image020.png"></a></td>
+			<td width="25%" valign="top" style="text-align:center"><a href="http://biodiversitylibrary.org/page/41056019" target="_blank"><img border=0 width=107 height=153 src="/images/examples/image022.png"></a><br>This ones a bit of a mix of map and diagram but if it has any geographic aspect to then classify as a map</td>
+			<td width="25%" valign="top" style="text-align:center"><a href="http://biodiversitylibrary.org/page/41056132" target="_blank"><img border=0 width=96 height=137 src="/images/examples/image024.png"><sub></sub></a></td>
+			<td width="25%" valign="top" style="text-align:center"><a href="http://biodiversitylibrary.org/page/36846752" target="_blank"><img border=0 width=82 height=143 src="/images/examples/image026.png"></a></td>
+		</tr>
+	</table>
+	
+	<h2 style="color:#4F7E97;margin-top:3em">Photographs</h2>
+	<p>This class includes still images produced from radiation-sensitive materials (sensitive to light, electron beams, or nuclear radiation), and therefore have a more 3 dimensional quality to them. </p>
+	<table cellspacing="0" cellpadding="5" align="center" width="50%">
+		<tr>
+			<td width="150" valign="top" style="text-align:center"><a href="http://biodiversitylibrary.org/page/41056090" target="_blank"><img border=0 width=89 height=127 src="/images/examples/image028.png"></a><br>See also: <a href=" http://biodiversitylibrary.org/page/21374833">this page</a></td>
+			<td width="150" valign="top" style="text-align:center"><a href="http://biodiversitylibrary.org/page/41056103" target="_blank"><img border=0 width=91 height=132 src="/images/examples/image032.png"></a><br>Micrographs would be included under this type. See also: <a href="http://biodiversitylibrary.org/page/41056103">This page</a></td>
+		</tr>
+	</table>
+
+	<p><b>Photographs and illustrations should also be identified as being"b&amp;w" or"color" </b></p>
+	<p>If a page contains any amount of color, even if minimal, it should be assigned to color </p>
+	<table cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td><img border=0 width=100 height=139 src="/images/examples/image034.png"></td>
+			<td><img border=0 width=94 height=130 src="/images/examples/image036.png"></td>
+		</tr>
+		<tr>
+			<td align="center">Color Illustration</td>
+			<td align="center">B&amp;W Illustration</td>
+		</tr>
+	</table>
+
+	<h2 style="color:#4F7E97;margin-top:3em">Bookplates</h2>
+	<table cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td style="text-align:center">
+				<img border=0 width=143 height=170 src="/images/examples/image038.png"><br>
+				<a href="https://www.google.com/search?q=bookplate&amp;espv=210&amp;es_sm=93&amp;tbm=isch&amp;imgil=jqoZ6TRqPYifoM%253A%253Bhttps%253A%252F%252Fencrypted-tbn1.gstatic.com%252Fimages%253Fq%253Dtbn%253AANd9GcRJRjplBP_MjQWDLa0ZbA9K57JRJl7AyqkEZvVE7OnBuRUpVrKe%253B1944%253B2592%253BdL35lQJfqCYmUM%253Bhttp%25253A%25252F%25252Fen.wikipedia.org%25252Fwiki%25252FBookplate&amp;source=iu&amp;usg=__Bezy8mIty-TSfAn7H78nfHEUDuw%3D&amp;sa=X&amp;ei=tvQEU_C9B8eEyAHYioCABQ&amp;ved=0CDgQ9QEwAQ&amp;biw=1024&amp;bih=719#facrc=_&amp;imgdii=jqoZ6TRqPYifoM%3A%3BYzOGiO_YcbF60M%3BjqoZ6TRqPYifoM%3A&amp;imgrc=jqoZ6TRqPYifoM%253A%3BdL35lQJfqCYmUM%3Bhttp%253A%252F%252Fupload.wikimedia.org%252Fwikipedia%252Fcommons%252F7%252F78%252FExlibris.JPG%3Bhttp%253A%252F%252Fen.wikipedia.org%252Fwiki%252FBookplate%3B1944%3B2592">Not from BHL - taken off Internet</a>
+			</td>
+		</tr>
+	</table>
 		
-		<h3 style="color:#4F7E97">Map</h3>
-		<p>Maps are any illustration that conveys geographic information and will usually be accompanied by a description or caption beginning with the word "map" but could also include "figure"  </p>
-		<p>The second one is a mix of map and diagram but if it has any geographic aspect to then it should be classified as a map.</a>
-		<table cellspacing="0" cellpadding="5" align="center">
-			<tr>
-				<td><a href="/images/examples-lg/map-1.jpg" target="_blank"><img src="/images/examples-sm/map-1.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/map-2.jpg" target="_blank"><img src="/images/examples-sm/map-2.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/map-3.jpg" target="_blank"><img src="/images/examples-sm/map-3.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/map-4.jpg" target="_blank"><img src="/images/examples-sm/map-4.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-			</tr>
-		</table>
-
-		<h3 style="color:#4F7E97">Photograph</h3>
-		<p>This class includes still images produced from radiation-sensitive materials (sensitive to light, electron beams, or nuclear radiation),  and therefore have a more 3 dimensional quality to them.  </p>
-		<p>Photographs should be identified as being “b&w” or “color”. If a page contains any amount of color, even if minimal, it should be assigned to color</p>
-		<table cellspacing="0" cellpadding="5" align="center">
-			<tr>
-				<td><a href="/images/examples-lg/photo-1.jpg" target="_blank"><img src="/images/examples-sm/photo-1.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/photo-2.jpg" target="_blank"><img src="/images/examples-sm/photo-2.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-				<td><a href="/images/examples-lg/photo-3.jpg" target="_blank"><img src="/images/examples-sm/photo-3.jpg" width="100" height="143" border="0" class="example-border"></a></td>
-			</tr>
-		</table>	
-
-<!-- 
-		<h3 style="color:#4F7E97">Bookplate</h3>
-
- -->
+	<h2 style="color:#4F7E97;margin-top:3em">Miscellaneous</h2>
+	<p>Pages that we do not consider as having an illustration or that we do not want to further describe &#8211; instead of giving them a type indicate"NO IMAGES ON THIS PAGE" </p>
+	<table cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td width="25%" style="text-align:center"><h4>Emblems</h4></td>
+			<td width="25%" style="text-align:center"><h4>Advertisements </h4></td>
+			<td width="25%" style="text-align:center"><h4>Book covers </h4></td>
+			<td width="25%" style="text-align:center"><h4>Indexes </h4></td>
+		</tr>
+		<tr>
+			<td valign="top" style="text-align:center"><img border=0 width=142 height=204 src="/images/examples/image040.png"><br>Emblems that are part of a logo at the beginning of every journal volume, shows up repeatedly in a journal run http://biodiversitylibrary.org/page/684364 </td>
+			<td valign="top" style="text-align:center"><img border=0 width=158 height=215 src="/images/examples/image042.png"></td>
+			<td valign="top" style="text-align:center"><img border=0 width=136 height=214 src="/images/examples/image044.png"></td>
+			<td valign="top" style="text-align:center"><img border=0 width=145 height=234 src="/images/examples/image046.png"></td>
+		</tr>
+	</table>
 	</div>
 </body>
 </html>
